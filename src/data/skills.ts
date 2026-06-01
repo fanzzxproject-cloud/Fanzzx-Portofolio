aed76755f69fc8be1b9ebd4761ee9082b074bc1a
fanzzxproject-cloud <fanzzxproject@gmail.com>
2026-06-01 18:12:32 +0700

    Create skills.ts

---
+++ src/data/skills.ts
@@ -0,0 +1,27 @@
+export const frontendSkills = [
+  { name: 'TypeScript', icon: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg' },
+  { name: 'HTML', icon: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg' },
+  { name: 'CSS', icon: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg' },
+  { name: 'JavaScript', icon: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png' },
+  { name: 'C++', icon: 'https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg' },
+  { name: 'C', icon: 'https://zlr.my.id/8fk7.png' },
+  { name: 'Tailwind', icon: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg' },
+  { name: 'React.js', icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' }
+];
+
+export const backendSkills = [
+  { name: 'Python', icon: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg' },
+  { name: 'Java', icon: 'https://zlr.my.id/Qv5J.png' },
+  { name: 'TypeScript', icon: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg' },
+  { name: 'PHP', icon: 'https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg' }
+];
+
+export const aiTools = [
+  { name: 'Project IDX', icon: 'https://zlr.my.id/ZBvS.jpeg' },
+  { name: 'Google AI Studio', icon: 'https://zlr.my.id/2q2d.jpeg' }
+];
+
+export const repoDeploy = [
+  { name: 'GitHub', icon: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' },
+  { name: 'Vercel', icon: 'https://assets.vercel.com/image/upload/v1588805858/repositories/vercel/logo.png' }
+];
